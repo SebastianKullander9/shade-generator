908a2eeeb250618589a4b85c819884a1d2bdd5e1
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-07-24 14:05:20 +0200

    Fix useSearchParams Suspense error in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { Element, scroller } from 'react-scroll';
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import SideBar from "./components/SideBar";
+import { Suspense } from "react";
 
 export default function Home() {
 	const [id, setId] = useState("");
@@ -45,44 +46,46 @@ export default function Home() {
 	const scrollToShades = () => scrollTo("section2");
 
 	return (
-		<div className="w-full">
-			<IconDark />
-			<SideBar id={id} projectName={projectNameState} scrollToShades={scrollToShades} />
-			
-			<Element name="section1" className="relative">
-				<div className="bg-turqoise-50 h-[calc(100vh-100px)] sm:h-[calc(100vh-200px)] flex justify-center items-center">
-					<ColorInput scrollToElement={() => scrollTo("section2")} />
-					<button onClick={() => scrollTo("section2")} className={`absolute bottom-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
-						<IoIosArrowDown size={40} />
-					</button>
-				</div>
-			</Element>
-			
-			<Element name="section2" className="relative">
-				<div className={`min-h-[100vh] bg-background ${colors.length === 0 ? "hidden" : ""}`}>
-					<GenerateShades />
-					<div className="flex justify-center">	
-						<button onClick={() => scrollTo("section1", -200)} className={`absolute top-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
-							<IoIosArrowUp size={40} />
-						</button>
-						<button onClick={() => scrollTo("section3")} className={`absolute bottom-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
+		<Suspense>
+			<div className="w-full">
+				<IconDark />
+				<SideBar id={id} projectName={projectNameState} scrollToShades={scrollToShades} />
+				
+				<Element name="section1" className="relative">
+					<div className="bg-turqoise-50 h-[calc(100vh-100px)] sm:h-[calc(100vh-200px)] flex justify-center items-center">
+						<ColorInput scrollToElement={() => scrollTo("section2")} />
+						<button onClick={() => scrollTo("section2")} className={`absolute bottom-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
 							<IoIosArrowDown size={40} />
 						</button>
 					</div>
-				</div>
-			</Element>
-			
-			<Element name="section3" className="relative">
-				<div className={`min-h-[100vh] bg-gold-50 ${colors.length === 0 ? "hidden" : ""}`}>
-					<GenerateCode />
-					<div className="flex justify-center">
-						<button onClick={() => scrollTo("section2")} className={`absolute top-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
-							<IoIosArrowUp size={40} />
-						</button>
+				</Element>
+				
+				<Element name="section2" className="relative">
+					<div className={`min-h-[100vh] bg-background ${colors.length === 0 ? "hidden" : ""}`}>
+						<GenerateShades />
+						<div className="flex justify-center">	
+							<button onClick={() => scrollTo("section1", -200)} className={`absolute top-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
+								<IoIosArrowUp size={40} />
+							</button>
+							<button onClick={() => scrollTo("section3")} className={`absolute bottom-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
+								<IoIosArrowDown size={40} />
+							</button>
+						</div>
 					</div>
-				</div>
-			</Element>
-			
-		</div>
+				</Element>
+				
+				<Element name="section3" className="relative">
+					<div className={`min-h-[100vh] bg-gold-50 ${colors.length === 0 ? "hidden" : ""}`}>
+						<GenerateCode />
+						<div className="flex justify-center">
+							<button onClick={() => scrollTo("section2")} className={`absolute top-4 right-4 cursor-pointer ${colors.length === 0 ? "hidden" : ""}`} >
+								<IoIosArrowUp size={40} />
+							</button>
+						</div>
+					</div>
+				</Element>
+				
+			</div>
+		</Suspense>
 	);
 }
